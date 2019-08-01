41dc4d10f9def77900e928f6dc718c8fba3a6e4f
mehdi <nima.mehdi@ensta-bretagne.org>
2019-08-01 21:54:27 +0200

    Added some elements for nodes handling

--- quadcopter/src/drone_dynamics/include/drone_dynamics/Quadopter.h
+++ quadcopter/src/drone_dynamics/include/drone_dynamics/Quadopter.h
@@ -4,30 +4,62 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Pose.h"
 #include "nodelet/nodelet.h"
-#include "eigen3/Eigen/Core"
+#include "nodelet_topic_tools/nodelet_throttle.h"
+#include <eigen3/Eigen/Core>
+#include <tf2_eigen>
 
 
 namespace quadcopter
 {
     class Quadcopter: public nodelet::Nodelet
     {
-        static constexpr double mass = 15.52;
+        static constexpr double mass = 2.3;
         static constexpr double arm_len = 0.25;  
 
         public:
             virtual void onInit();
-
-
+        
+        void update_dynamics()
+        void compute_rotation_matrix();
+        
+        struct StateVector
+        {
+            Eigen::Vector3d pose // x,y,z
+            Eigen::Vector3d velocity; // vx vy vz
+            Eigen::Vector3d angular_vel;// p q r
+            Eigen::Vector4d quaternion; // qw qx qy qz
+        };
+        
 
 
         private:
 
-        Eigen::Vector3d linear_accel;
-        Eigen::Vector3d angular_accel;
-        Eigen::Vector3d angular_rate;
-        Eigen::Vector3d velocity;
+        // callback methods
+        void callback(const geometry_msgs::QuaternionConstPtr& input);
+        void callback2(const std_msgs::Float64Ptr input);
+
+
+        // nodelet stuff
+        ros::NodeHandle private_nh;
+        ros::Subscriber _sub;
+        ros::Publisher _pub;
+
+
+        // state vector
+        // [x y z vx vy vz qw qx qy qz p q r]
+        StateVector state;
+        StateVector derivative;
+        Eigen::Matrix3d rotation_matrix;
+
         
+        // Inertial coef
+        double Ixx;
+        double Iyy;
+        double Izz;
+
+        //
         Eigen::Vector4d thrust;
+
 
 
 
---
+++ quadcopter/src/drone_dynamics/src/Quadcopter.cpp
@@ -0,0 +1,23 @@
+#include "Quadopter.h"
+
+namespace quadcopter
+{
+    void Quadcopter::onInit()
+    {
+        NODELET_DEBUG("initializing nodelet");
+        // get the Node Handler
+        &private_nh = getPrivateNodeHandle;
+    }
+
+    Quadcopter::compute_rotation_matrix()
+    {
+        // TODO
+    }
+
+
+    Quadcopter::update_dynamics()
+    {
+        // TODO
+    }   
+
+}
