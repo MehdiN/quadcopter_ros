f853e4dc0da7799e3ede7e700ce37ff7c3748f83
MehdiN <8954670+MehdiN@users.noreply.github.com>
2019-10-23 22:33:03 +0200

    Use the wrapper class instead of the pid

--- controller/src/controller.cpp
+++ controller/src/controller.cpp
@@ -7,13 +7,7 @@ Main control loop for drone control
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
 #include "geometry_msgs/Pose.h"
-#include "controller/PID.h"
-
-
-void poseCallback(const geometry_msgs::Pose::ConstPtr& pose)
-{
-    float altitude = pose->position.z;
-}
+#include "AltitudeController.h"
 
 
 int main(int argc, char **argv)
@@ -22,16 +16,18 @@ int main(int argc, char **argv)
     ros::init(argc,argv,"pub_control");
     ros::NodeHandle node;
 
+    AltitudeController altitude_controller;
+
+    // Altitude controller;
+    altitude_controller.init();
+    altitude_controller.load_parameters();
+
+    // initialize the subcriber and the publisher
     ros::Publisher control_pub = node.advertise<std_msgs::Float32>("cmd_z",1);
-    ros::Subscriber control_pub = node.subscribe("/quadcopter/pose",1,poseCallback);
+    ros::Subscriber control_pub = node.subscribe("/quadcopter/pose",1,AltitudeController::callback,&altitude_controller);
 
     ros::Rate rate(1);
 
-    // Altitucde Controller 
-    PID::PID pid;
-    pid.init_or_reset_PID();
-    pid.load_pid_params();
-    
     double z_target = -10; // In NED configuration
     std_msgs::Float32 command;
 
@@ -39,10 +35,10 @@ int main(int argc, char **argv)
     while( ros::ok())
     {
         
-
-        // Simple PID for altitude control
-        pid.set_target(z_target);
-        command.data = pid.get_pid();
+        // 
+        altitude_controller.set_target(z_target);
+        altitude_controller.run();
+        command.data = altitude_controller.get_cmd();
         
         control_pub.publish(command);
         ros::spinOnce();
